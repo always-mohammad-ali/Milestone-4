970c9a4333e801ed4d162e803d9a6d607ccdce26
Mohammad Ali <always.mohammad.ali@gmail.com>
2025-05-30 14:51:19 +0600

    initial repository

---
+++ module-17/first.js
@@ -0,0 +1,36 @@
+console.log(2224)
+
+console.log(22*2)
+
+let myname = "ali after the name of greatest fighter of all time";
+
+console.log(myname)
+
+let age= 20;
+
+let celebrate = "happy birthday to your special day february";
+
+console.log(celebrate, age)
+
+console.log(typeof age)
+
+let years = true;
+console.log(years)
+
+// hey man are you learning js?
+console.log(typeof myname)
+
+let fraction = 0.1;
+let fraction2 = 0.2;
+
+
+let yearss = '22';
+console.log(parseint(yearss))
+
+// console.log(tofixed(fraction + fraction2))
+
+// let decimal = parseint('22');
+ 
+// let total = decimal + age;
+
+// console.log(total)
---
+++ module-17/simplemath.js
@@ -0,0 +1,8 @@
+const totalGiven = 1000;
+
+const apple =100;
+const orange =500;
+
+const total = apple + orange;
+
+console.log("You will get", totalGiven-total)
